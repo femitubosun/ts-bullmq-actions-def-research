9c64e5771d62ee1186ce2d381dacf3424d7efeab
Femi Olatubosun | ƒa†3 <thefemiolatubosun@gmail.com>
2025-07-09 01:26:41 +0100

    feat: sort files

---
+++ src/action.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+import { A } from "./helpers/action";
+
+export const actionDef = {
+  create: A("documentation.create")
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        sourceUrl: z.string(),
+      }),
+    )
+    .output(
+      z.object({
+        id: z.string(),
+        title: z.string(),
+        description: z.string(),
+        sourceUrl: z.string(),
+      }),
+    ),
+  // .settings(),
+};
